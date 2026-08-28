3876761dc477b679ebf2f4a3fbc5c88caf92f627
dashmilad <33802714+dashmilad@users.noreply.github.com>
2026-08-28 16:20:28 +0330

    delete factor

--- src/main.jsx
+++ src/main.jsx
@@ -1113,14 +1113,6 @@ function App() {
   </select>
 
   <input
-    value={row.invoice}
-    onChange={e =>
-      updateRow(index, 'invoice', e.target.value)
-    }
-    placeholder="شماره فاکتور"
-  />
-
-  <input
     className="description-input"
     value={row.description}
     onChange={e =>
